703b360f356981d2d7b7901e0c7c4356c0905215
mashikos4 <mashikos4@gmail.com>
2020-10-26 21:04:15 +0200

    Add files via upload

---
+++ Welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="uif-8">
+<title> login page</title>
+</head>
+<body>
+<?php
+session_start();
+?>
+<h1 align ="center"> Welcome to my profile page</h1>
+<p align="center"><a href="logout.php">logout</a></p>
+</body>
+</html>
